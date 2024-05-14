f467e666cff31b48a077bd85335febae4a9af7e6
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-05-14 20:05:04 +0200

    delete

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,25 @@
 import GlobalStyle from "../styles";
-import { useState } from "react";
+import { useLocalStorageState } from "react"; // Import useLocalStorageState hook
 import { initialBooks } from "@/lib/books";
+import { fetchBooks } from "@/lib/api";
+import { useEffect } from "react";
 
-export default function App({ Component, pageProps }) {
-  const [books, setBooks] = useState(initialBooks);
+export default async function App({ Component, pageProps }) {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const fetchedBooks = await fetchBooks();
+        setBooks(fetchedBooks);
+      } catch (error) {
+        console.error("Error fetching books:", error);
+        // Handle error in UI if needed (e.g., display an error message)
+      }
+    };
+
+    fetchData();
+  }, []);
 
   function handleAddBook(newBook) {
     setBooks([...books, newBook]);
