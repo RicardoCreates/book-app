ce2d166e045fcf960b728bd47fa096f29d7ceeed
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-05-18 23:16:08 +0200

    try

--- pages/api/books.js
+++ pages/api/books.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
         res.status(405).end(`Method ${req.method} Not Allowed`);
     }
   } catch (error) {
-    console.error("Database connection error:", error);
+    console.error("Database connection error:", error.message, error.stack);
     res.status(500).json({ error: "Database connection error" });
   }
 }
